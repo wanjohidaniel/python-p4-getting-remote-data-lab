50882d377d60002bd7d375bba7add70c487b91fc
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-06-28 00:16:28 +0300

    Getting Remote Data Lab (CodeGrade)

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()  # Ensure we raise an exception for bad responses
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return requests.get(self.url).json()
